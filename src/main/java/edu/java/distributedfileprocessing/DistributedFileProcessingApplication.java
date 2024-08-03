ea5bc731796fedea119e0f8eae0ab7db44c61991
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-08-03 17:05:11 +0300

    init commit

---
+++ src/main/java/edu/java/distributedfileprocessing/DistributedFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package edu.java.distributedfileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedFileProcessingApplication.class, args);
+	}
+
+}
