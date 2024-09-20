83d790d64fdb8a9f0534013636091918946486bb
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-09-21 01:13:40 +0500

    update docker-compose.yml

--- src/main/java/edu/java/distributedfileprocessing/config/AppConfig.java
+++ src/main/java/edu/java/distributedfileprocessing/config/AppConfig.java
@@ -1,15 +1,10 @@
 package edu.java.distributedfileprocessing.config;
 
-import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import io.minio.errors.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Clock;
 
 @Configuration
@@ -28,22 +23,10 @@ public class AppConfig {
     @Bean
     public MinioClient minioClient(AppProperties appProperties) {
         AppProperties.MinioProperties props = appProperties.getMinio();
-        MinioClient minioClient = MinioClient.builder()
+        return MinioClient.builder()
                 .endpoint(props.getUrl())
                 .credentials(props.getAccessKey(), props.getSecretKey())
                 .build();
-        try {
-            minioClient.makeBucket(
-                    MakeBucketArgs
-                            .builder()
-                            .bucket(props.getBucketName())
-                            .build());
-        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
-                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
-                 XmlParserException e) {
-            throw new RuntimeException(e);
-        }
-        return minioClient;
     }
 
 }
